fd6ac5fb3c0e1171c53338154c6b4b2cee9e4e16
Dang Quang <quangthat97@gmail.com>
2023-06-09 09:33:22 +0700

    First Me

--- utils/db.js
+++ utils/db.js
@@ -1,25 +1,26 @@
 import mongoose from 'mongoose';
- 
+
 let isConnected = false;
 
 export const connectDB = async () => {
-  mongoose.set('strictQuery', true)
+	mongoose.set('strictQuery', true);
 
-  if(isConnected) {
-    console.log('MongoDb is already connected');
-    return 
-  };
+	if (isConnected) {
+		console.log('MongoDB is already connected');
+		return;
+	}
 
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share_prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+	try {
+		await mongoose.connect(process.env.MONGODB_URI, {
+			dbName: 'share_prompt',
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 
-    isConnected = true
-    console.log("MongoDB connect")
-  } catch (error) {
-    console.log(error)
-  }
-}
+		isConnected = true;
+
+		console.log('MongoDB connected');
+	} catch (error) {
+		console.log(error);
+	}
+};
